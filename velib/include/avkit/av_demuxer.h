9624c1df4880ca9e33af5cf3ae6732dc60a11042
xsmart <xsmart@163.com>
2014-12-13 23:23:38 +0800

    add jpeg decoder

--- velib/include/avkit/av_demuxer.h
+++ velib/include/avkit/av_demuxer.h
@@ -8,6 +8,7 @@
 #include "cppkit/ck_nullable.h"
 #include <utility>
 #include <vector>
+#include <map>
 
 extern "C"
 {
@@ -43,15 +44,20 @@ struct stream_info
 {
     stream_type type;
     int index;
+    enum AVCodecID     codec_id; /* see AV_CODEC_ID_xxx */
+    int width;
+    int height;
 };
 
 class h264mp4_to_annexb;
 class h264_decoder;
+class jpeg_decoder;
 
 class av_demuxer
 {
     friend class h264mp4_to_annexb;
     friend class h264_decoder;
+    friend class jpeg_decoder;
 
 public:
     CK_API av_demuxer( const cppkit::ck_string& fileName, bool annexBFilter = true );
@@ -68,10 +74,11 @@ public:
 
     CK_API cppkit::ck_string get_file_name() const;
 
-    CK_API double get_seconds_between_frames( int streamIndex ) const;
+    CK_API double get_seconds_between_frames(int streamIndex ) const;
     CK_API std::pair<int,int> get_time_base( int streamIndex ) const;
 
-    CK_API std::vector<stream_info> get_stream_types() const;
+    CK_API bool get_stream_types(int streamIndex, 
+		struct stream_info &p_stream_info);
     CK_API int get_video_stream_index() const;
     CK_API int get_primary_audio_stream_index() const;
 
@@ -109,7 +116,7 @@ private:
     bool _eof;
     AVPacket _deMuxPkt;
     AVPacket _filterPkt;
-    std::vector<stream_info> _streamTypes;
+    std::map<int , struct stream_info> _streamTypes;
     int _videoStreamIndex;
     int _audioPrimaryStreamIndex;
     AVBitStreamFilterContext* _bsfc;
