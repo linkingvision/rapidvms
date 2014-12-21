ec01f7697dc5526f6b94bd91ed24455e9950dd2a
xsmart <xsmart@163.com>
2014-12-21 12:53:59 +0800

    add datamining

--- veapp/main/vscdevicelist.h
+++ veapp/main/vscdevicelist.h
@@ -65,6 +65,7 @@ public slots:
 	void SurveillanceClick();
 	void CameraAddClick();
 	void EmapClick();
+	void DminingClick();
 	void SearchClick();
 	void RecorderClick();
 
@@ -79,6 +80,7 @@ public slots:
 signals:
 	void SurveillanceClicked();
 	void EmapClicked();
+	void DminingClicked();
 	void SearchClicked();
 	void RecorderClicked();
 
