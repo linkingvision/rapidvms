2abc95c7e4857c436f9d3659d09c621df6f26241
xsmart <xsmart@163.com>
2014-12-28 21:51:56 +0800

    change ui

--- veapp/main/vscipcgroupconf.cpp
+++ veapp/main/vscipcgroupconf.cpp
@@ -10,7 +10,11 @@ VSCIPCGroupConf::VSCIPCGroupConf(QWidget *parent)
    ui.setupUi(this);
    connect(ui.pbSave, SIGNAL(clicked()), this, SLOT(SaveClicked()));
    connect(ui.pbCancel, SIGNAL(clicked()), this, SLOT(CancelClicked()));
-
+#ifdef WIN32
+	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::Tool);
+#else
+	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::ToolTip);
+#endif
 }
 
 void VSCIPCGroupConf::SaveClicked()
@@ -21,6 +25,29 @@ void VSCIPCGroupConf::CancelClicked()
 {
 	m_Type = VSC_IPCGROUP_CONF_CANCEL;
 }
+
+	
+void VSCIPCGroupConf::mousePressEvent(QMouseEvent *event)  
+{  
+    if (event->button() == Qt::LeftButton) {  
+        m_Drag = true;  
+        m_DragPosition = event->globalPos() - this->pos();  
+        event->accept();  
+    }  
+}  
+  
+void VSCIPCGroupConf::mouseMoveEvent(QMouseEvent *event)  
+{  
+    if (m_Drag && (event->buttons() && Qt::LeftButton)) {  
+        move(event->globalPos() - m_DragPosition);  
+        event->accept();  
+    }  
+}  
+  
+void VSCIPCGroupConf::mouseReleaseEvent(QMouseEvent *)  
+{  
+    m_Drag = false;  
+} 
 
 
 
