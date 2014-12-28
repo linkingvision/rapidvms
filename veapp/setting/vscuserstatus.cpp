86d080d224904d149fc7ab8a6000bc42377413c2
xsmart <xsmart@163.com>
2014-12-28 21:47:43 +0800

    change ui

--- veapp/setting/vscuserstatus.cpp
+++ veapp/setting/vscuserstatus.cpp
@@ -10,9 +10,11 @@ VSCUserStatus::VSCUserStatus(QWidget *parent)
 
    //connect(ui.pbSave, SIGNAL(clicked()), this, SLOT(SaveClicked()));
    //connect(ui.pbCancel, SIGNAL(clicked()), this, SLOT(CancelClicked()));
-#ifndef WIN32
+#ifdef WIN32
+	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::Tool);
+#else
 	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::ToolTip);
-#endif	
+#endif
 
 }
 
@@ -24,6 +26,29 @@ void VSCUserStatus::CancelClicked()
 {
 	//m_Type = VSC_IPCGROUP_CONF_CANCEL;
 }
+
+
+void VSCUserStatus::mousePressEvent(QMouseEvent *event)  
+{  
+    if (event->button() == Qt::LeftButton) {  
+        m_Drag = true;  
+        m_DragPosition = event->globalPos() - this->pos();  
+        event->accept();  
+    }  
+}  
+  
+void VSCUserStatus::mouseMoveEvent(QMouseEvent *event)  
+{  
+    if (m_Drag && (event->buttons() && Qt::LeftButton)) {  
+        move(event->globalPos() - m_DragPosition);  
+        event->accept();  
+    }  
+}  
+  
+void VSCUserStatus::mouseReleaseEvent(QMouseEvent *)  
+{  
+    m_Drag = false;  
+} 
 
 
 
