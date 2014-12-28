d03f1523798bbb22ef8b0a57fc5fedba558007fe
xsmart <xsmart@163.com>
2014-12-28 21:48:12 +0800

    change ui

--- veapp/main/vscviewconf.cpp
+++ veapp/main/vscviewconf.cpp
@@ -9,6 +9,11 @@ VSCViewConf::VSCViewConf(QWidget *parent)
    ui.setupUi(this);
    connect(ui.pbNew, SIGNAL(clicked()), this, SLOT(NewClicked()));
    connect(ui.pbModify, SIGNAL(clicked()), this, SLOT(ModifyClicked()));
+#ifdef WIN32
+	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::Tool);
+#else
+	setWindowFlags(Qt::FramelessWindowHint|Qt::NoDropShadowWindowHint| Qt::ToolTip);
+#endif
 
 }
 
@@ -20,6 +25,28 @@ void VSCViewConf::ModifyClicked()
 {
 	m_Type = VSC_VIEW_CONF_MODIFY;
 }
+
+void VSCViewConf::mousePressEvent(QMouseEvent *event)  
+{  
+    if (event->button() == Qt::LeftButton) {  
+        m_Drag = true;  
+        m_DragPosition = event->globalPos() - this->pos();  
+        event->accept();  
+    }  
+}  
+  
+void VSCViewConf::mouseMoveEvent(QMouseEvent *event)  
+{  
+    if (m_Drag && (event->buttons() && Qt::LeftButton)) {  
+        move(event->globalPos() - m_DragPosition);  
+        event->accept();  
+    }  
+}  
+  
+void VSCViewConf::mouseReleaseEvent(QMouseEvent *)  
+{  
+    m_Drag = false;  
+} 
 
 
 
