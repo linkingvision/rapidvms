ba810f62858368b7cb6b592349774e0995203ac7
xsmart <xsmart@163.com>
2015-01-03 16:30:31 +0800

    update hdfs

--- velib/include/vdb/hdfsrecsession.hpp
+++ velib/include/vdb/hdfsrecsession.hpp
@@ -2,25 +2,20 @@
 #define __HDFS_RECORD_SESSION_HPP__
 #include "utility.hpp"
 
-#include "cppkit/ck_memory.h"
-#include "cppkit/ck_command_queue.h"
-#include "avkit/av_muxer.h"
 
 using namespace std;
-using namespace cppkit;
-using namespace avkit;
 
 class VHdfsDB;
-class HdfsRecSession
+class HdfsRecSessionData;
+class VE_LIBRARY_API HdfsRecSession
 {
 public:
-    HdfsRecSession(s32 deviceId, astring strName, VHdfsDB &pDB);
-    ~HdfsRecSession();
+	HdfsRecSession(s32 deviceId, astring strName, VHdfsDB &pDB);
+	~HdfsRecSession();
 public:
 	MFStatus PushAFrame(VideoFrame *pFrame);
 private:
-    s32 m_DeviceId;
-	VHdfsDB &m_pDB;
+    	HdfsRecSessionData * m_data;
 };
 
 #endif /* __HDFS_RECORD_SESSION_HPP__ */
