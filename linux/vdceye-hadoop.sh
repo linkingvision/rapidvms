16b6a19c7486271f7a9906fb69172161bd902226
xsmart <xsmart@163.com>
2015-01-17 00:06:50 +0800

    update linux hadoop vdceye shell

--- linux/vdceye-hadoop.sh
+++ linux/vdceye-hadoop.sh
@@ -4,14 +4,9 @@ export JAVA_HOME=/home/user/work/hdp/jdk1.8.0_25/
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server/
 
-CLASSPATH=./
-for f in $HADOOP_HOME/*.jar; do
-  CLASSPATH=${CLASSPATH}:$f;
-done
-for f in $HADOOP_HOME/lib/*.jar; do
-  CLASSPATH=${CLASSPATH}:$f;
-done
-export CLASSPATH=$CLASSPATH
+CLASSPATH=${CLASSPATH}":"$JAVA_HOME\lib":"$JAVA_HOME\lib\dt.jar":"$JAVA_HOME\lib\tools.jar":"`find ${HADOOP_HOME}/share/hadoop | awk '{path=path":"$0}END{print path}'` # jar of hadoop
+export CLASSPATH
+#echo $CLASSPATH
 
 mkdir -p ./ve/videodb/
 
