5c68080e883ef3aac5cde96619ada37a4c1b5b0e
xsmart <xsmart@163.com>
2015-01-31 23:24:07 +0800

    add mining framework

---
+++ mining/include/mframework.hpp
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// File: mframework.hpp
+//
+// Desc: Data Mining Framework.
+//
+// Copyright (c) 2014-2018. veyesys.com All rights reserved.
+//------------------------------------------------------------------------------
+#ifndef __M_FRAME_WORK_HPP__
+#define __M_FRAME_WORK_HPP__
+#include "utility.hpp"
+#include "debug.hpp"
+#include "videotype.hpp"
+#include "miningtype.hpp"
+
+/* All the mining data will be post a queue to here,
+    Then start a thread to peek the queue to post to each device in factory
+*/
+
+
+#endif /* __M_FRAME_WORK_HPP__ */
