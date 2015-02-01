125e31158d1e2eb276ecda06c0d49de0166b34b1
xsmart <xsmart@163.com>
2015-02-01 15:58:46 +0800

    update mining framework

--- mining/include/mframework.hpp
+++ mining/include/mframework.hpp
@@ -11,10 +11,80 @@
 #include "debug.hpp"
 #include "videotype.hpp"
 #include "miningtype.hpp"
+#include "mmodule.hpp"
+#include <QThread>
+#include <qdebug.h>
+#include "cppkit/ck_string.h"
+#include "cppkit/ck_memory.h"
+#include "cppkit/ck_command_queue.h"
+#include "cppkit/ck_dynamic_library.h"
+#include "cppkit/ck_byte_ptr.h"
+
+using namespace std;
+using namespace cppkit;
 
 /* All the mining data will be post a queue to here,
     Then start a thread to peek the queue to post to each device in factory
 */
 
+typedef command_queue<MiningRet> MModuleRetQueue;
+
+typedef std::map<int, MiningModule *> MModuleMap;
+class MFramework:public QThread
+{
+    Q_OBJECT
+public:
+	inline MFramework(astring strPath);
+	inline ~MFramework();
+public:
+	inline void run();
+public:
+	inline static BOOL RetHandler(s32 id, MiningRet& ret, void * pParam);
+	inline BOOL RetHandler1(s32 id, MiningRet& ret);
+		
+private:
+	MModuleMap m_MModules;
+	MModuleRetQueue m_RetQueue;
+	BOOL m_bExit;
+};
+
+
+BOOL MFramework::RetHandler(s32 id, MiningRet& ret, void * pParam)
+{
+	int dummy = errno;
+	MFramework *pMFramework = (MFramework)pParam;
+
+	if (pMFramework)
+	{
+		return pMFramework->RetHandler1(id, ret);
+	}
+
+	return TRUE;
+}
+
+BOOL MFramework::RetHandler1(s32 id, MiningRet& ret)
+{
+	m_RetQueue.post(ret);
+
+	return TRUE;
+}
+
+void MFramework::run()
+{
+	MiningRet ret;
+	while (m_bExit != TRUE)
+	{
+		if (m_RetQueue.size() > 0)
+		{
+			ret = m_RetQueue.pop();
+			//send the ret va data to factory for showing
+			continue;
+		}else
+		{
+			ve_sleep(1000);
+		}
+	}
+	return;
+}
 
 #endif /* __M_FRAME_WORK_HPP__ */
--- mining/include/mmodule.hpp
+++ mining/include/mmodule.hpp
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// File: Module.hpp
+// File: mmodule.hpp
 //
 // Desc: Module for Data Mining.
 //
@@ -17,12 +17,24 @@
 #include "cppkit/ck_dynamic_library.h"
 #include "cppkit/ck_byte_ptr.h"
 
+using namespace std;
+using namespace cppkit;
+
 typedef enum __MiningRetType
 {
 	MINING_RET_MOT = 1,
 	MINING_RET_OBJS,
 	MINING_RET_LAST
 }MiningRetType;
+
+typedef enum __MModuleFlag
+{
+	MM_MOT = 1,
+	MM_TRACK = 2,
+	MM_PEOPLE_CNT = 4,
+	MM_ALPR = 8,
+	MM_LAST
+}MModuleFlag;
 
 typedef struct __MiningRet
 {
@@ -32,7 +44,6 @@ typedef struct __MiningRet
 		VeMotionBox mot;
 	} data;	
 }MiningRet;
-
 
 #ifdef WIN32
 typedef void (__cdecl * MiningCallbackFunctionPtr)(s32 id, MiningRet& ret, void * pParam);
@@ -52,6 +63,7 @@ public:
 	virtual BOOL Process(s32 id, RawFrame& frame) = 0;
 	virtual BOOL RegRetCallback(MiningCallbackFunctionPtr pCallback, void * pParam);
 	virtual BOOL UnRegDataCallback(void * pParam);
+	virtual u32 GetFlags();
 };
 
 /* Create MiningInterface from the module  */
@@ -104,8 +116,18 @@ public:
 		}
 		return FALSE;
 	}
+	inline u32 GetFlags()
+	{
+		if (m_module)
+		{
+			return m_module->GetFlags();
+		}
+		return FALSE;
+	}
+		
 private:
 	MiningInterface * m_module;
+	ck_dynamic_library m_dynLib;
 };
 
 
