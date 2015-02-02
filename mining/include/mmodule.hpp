6856277049489c9c2a68d705fd7e6dbe1c28b399
xsmart <xsmart@163.com>
2015-02-02 21:31:21 +0800

    update mmodule

--- mining/include/mmodule.hpp
+++ mining/include/mmodule.hpp
@@ -20,55 +20,7 @@
 using namespace std;
 using namespace cppkit;
 
-typedef enum __MiningRetType
-{
-	MINING_RET_MOT = 1,
-	MINING_RET_OBJS,
-	MINING_RET_LAST
-}MiningRetType;
 
-typedef enum __MModuleFlag
-{
-	MM_MOT = 1,
-	MM_TRACK = 2,
-	MM_PEOPLE_CNT = 4,
-	MM_ALPR = 8,
-	MM_LAST
-}MModuleFlag;
-
-typedef struct __MiningRet
-{
-	MiningRetType ret;
-	union {
-		VeObjGroup objs;
-		VeMotionBox mot;
-	} data;	
-}MiningRet;
-
-#ifdef WIN32
-typedef void (__cdecl * MiningCallbackFunctionPtr)(s32 id, MiningRet& ret, void * pParam);
-#else
-typedef void ( * MiningCallbackFunctionPtr)(s32 id, MiningRet& ret, void * pParam);
-#endif
-
-class MiningInterface
-{
-public:
-	MiningInterface() {}
-	virtual ~MiningInterface() {}
-
-public:
-	virtual BOOL AddChannel(s32 id) = 0;
-	virtual BOOL DelChannel(s32 id) = 0;
-	virtual BOOL Process(s32 id, RawFrame& frame) = 0;
-	virtual BOOL RegRetCallback(MiningCallbackFunctionPtr pCallback, void * pParam);
-	virtual BOOL UnRegDataCallback(void * pParam);
-	virtual u32 GetFlags();
-	virtual astring GetVersion();
-};
-
-/* Create MiningInterface from the module  */
-typedef MiningInterface * (*MiningCreateObject)();
 
 
 class MiningModule
