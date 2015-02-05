1c13c0115de9904aa602a1a4b00e4c466c433cc3
xsmart <xsmart@163.com>
2015-02-05 22:23:16 +0800

    add common http server

---
+++ devices/include/cmnserver.hpp
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// File: cmnserver.hpp
+//
+// Desc: Common HTTP server - HTTP server for all application
+//
+// Copyright (c) 2014-2018 opencvr.com. All rights reserved.
+//------------------------------------------------------------------------------
+#ifndef __VSC_CMN_SERVER__H_
+#define __VSC_CMN_SERVER__H_
+#include "utility.hpp"
+#include "debug.hpp"
+
+/* each app can register a http REST path can move 
+   the socket to itself, for streaming, it it for performance.
+*/
+
+#endif /* __VSC_CMN_SERVER__H_ */
+
