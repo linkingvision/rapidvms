467ed1fa3432fa40341534e33ce5742c10ec3f06
xsmart <xsmart@163.com>
2015-02-10 22:47:08 +0800

    add lang setting

---
+++ veapp/setting/vsclanguage.cpp
@@ -0,0 +1,50 @@
+#include "vsclanguage.h"
+#include "hdddevice.hpp"
+#include "debug.hpp"
+#include "vschddone.h"
+#include "factory.hpp"
+
+extern Factory *gFactory;
+
+VSCLanguage::VSCLanguage(QWidget *parent)
+    : QWidget(parent)
+{
+	ui.setupUi(this);
+
+	gFactory->GetLang(m_lang);
+	connect( this->ui.pushButtonApply, SIGNAL( clicked() ), this, SLOT(applyConfig()));
+	SetupValue();
+}
+void VSCLanguage::SetupValue()
+{
+    switch(m_lang)
+    {
+	case VSC_LANG_EN:
+        ui.radioButtonEN->setChecked(true);
+        break;
+	case VSC_LANG_ZH:
+        ui.radioButtonZH->setChecked(true);
+        break;
+    default:
+        return;
+    }
+}
+
+
+
+void VSCLanguage::applyConfig()
+{
+	if (ui.radioButtonEN->isChecked() == true)
+	{
+		m_lang = VSC_LANG_EN;
+	}else if (ui.radioButtonZH->isChecked() == true)
+	{
+		m_lang = VSC_LANG_ZH;
+	}
+
+	gFactory->SetLang(m_lang);
+	return;
+
+}
+
+
