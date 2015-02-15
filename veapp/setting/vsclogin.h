bc8c4d8af382c054f10acb73ec2bbd81bad61d77
xsmart <xsmart@163.com>
2015-02-15 22:29:20 +0800

    add get passwd

--- veapp/setting/vsclogin.h
+++ veapp/setting/vsclogin.h
@@ -19,16 +19,29 @@ public:
 	void mouseMoveEvent(QMouseEvent *event);
 	void mouseReleaseEvent(QMouseEvent *event);
 public:
+	BOOL GetIsLogin()
+	{
+		return m_isLogin;
+	}
+	BOOL SetDefault()
+	{
+		m_isLogin = FALSE;
+		return TRUE;
+	}
+	BOOL GetUserPasswd(astring &strUser, astring &strPasswd);
 
 public slots: 
-	void SaveClicked();
-	void CancelClicked();
+	void LoginClicked();
+	void ExitClicked();
 
 public:
 	Ui::VSCLogin ui;
 private:
 	QPoint m_DragPosition;
 	bool m_Drag;
+
+private:
+	BOOL m_isLogin;
 	
 };
 
