6c2f5d746033baf6c0bfe82b5b870425182e92f0
xsmart <xsmart@163.com>
2015-03-07 21:39:53 +0800

    add oapi conf

--- veapp/setting/vscoapi.cpp
+++ veapp/setting/vscoapi.cpp
@@ -1,15 +1,26 @@
 #include "vscoapi.h"
 #include "debug.hpp"
 #include "factory.hpp"
+#include "cppkit/ck_string.h"
+#include "cppkit/os/ck_sprintf.h"
+
+using namespace cppkit;
+using namespace std;
 
 extern Factory *gFactory;
 
 VSCOAPI::VSCOAPI(QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent), m_port(9080)
 {
 	ui.setupUi(this);
-	//ui.user->setText("admin");
-	//ui.passwd->setText("admin");
+	
+	gFactory->GetOAPIPort(m_port);
+
+	ck_string ckPort = ck_string::from_uint16(m_port, 10); 
+
+	ui.OAPIPort->setText(ckPort.c_str());
+
+	connect( this->ui.pushButtonApply, SIGNAL( clicked() ), this, SLOT(applyConfig()));
 }
 
 
@@ -17,6 +28,11 @@ VSCOAPI::VSCOAPI(QWidget *parent)
 
 void VSCOAPI::applyConfig()
 {
+	ck_string ckPort = ui.OAPIPort->text().toStdString();
+
+	m_port = ckPort.to_uint16(10);
+
+	gFactory->SetOAPIPort(m_port);
 
 	return;
 
