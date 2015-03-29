021354dc21bbc0c09e978b2c59318a0dc339f08d
xsmart <xsmart@163.com>
2015-03-29 21:46:40 +0800

    add https server

--- devices/include/cmnserverimpl.hpp
+++ devices/include/cmnserverimpl.hpp
@@ -20,24 +20,14 @@ CmnHttpServer::~CmnHttpServer()
 
 void CmnHttpServer::run()
 {
-#define  CMN_SSL_SOCKET
-#ifndef CMN_SSL_SOCKET
         XSocket socket;
-#else
-        XSSLSocket socket;
-        socket.UsePEMCertificateFile( "C:\\videodb\\ServerCRT1.crt" );
-        socket.UsePEMRSAPrivateKeyFile( "C:\\videodb\\PrivateKey1.key" );
-#endif
+
         socket.Bind(m_port);
         socket.Listen();
 
 	while(1)
 	{
-#ifndef CMN_SSL_SOCKET
 		XRef<XSocket> clientSocket = socket.Accept();
-#else
-		XRef<XSocket> clientSocket = socket.Accept();
-#endif
 
 		ServerSideRequest request;
 		try
@@ -62,9 +52,63 @@ void CmnHttpServer::run()
 		catch(XSDK::XException)
 		{
 		}
-#ifndef CMN_SSL_SOCKET
+		
 		clientSocket->Shutdown(SOCKET_SHUT_FLAGS);
-#endif
+		clientSocket->Close();
+	}
+}
+
+
+
+
+/* Below is HTTPS */
+CmnHttpsServer::CmnHttpsServer(u16 port)
+:m_port(port)
+{
+
+}
+CmnHttpsServer::~CmnHttpsServer()
+{
+
+}
+
+void CmnHttpsServer::run()
+{
+        XSSLSocket socket;
+        socket.UsePEMCertificateFile( "C:\\videodb\\ServerCRT1.crt" );
+        socket.UsePEMRSAPrivateKeyFile( "C:\\videodb\\PrivateKey1.key" );
+
+        socket.Bind(m_port);
+        socket.Listen();
+
+	while(1)
+	{
+		XRef<XSocket> clientSocket = socket.Accept();
+
+		ServerSideRequest request;
+		try
+		{
+			request.ReadRequest(clientSocket);
+
+			const URI uri = request.GetURI();
+
+			XString body = "<!DOCTYPE html>\
+			<html>\
+			<head ><title>OpenCVR</title></head>\
+			<body>\
+			<h1>Wellcome OpenCVR //TODO</h1><br>\
+			</body>\
+			</html>";
+
+			ServerSideResponse ssResponse;
+			ssResponse.SetBody(body);
+			ssResponse.SetContentType("text/html; charset=UTF-8");
+			ssResponse.WriteResponse(clientSocket);
+		}
+		catch(XSDK::XException)
+		{
+		}
+		
 		clientSocket->Close();
 	}
 }
