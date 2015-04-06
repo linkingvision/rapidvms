dc32758cc349bbb6a6f926fe8d167ee3614f5412
xsmart <xsmart@163.com>
2015-04-06 22:17:15 +0800

    add oapi

---
+++ oapi/oapi.hpp
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// File: oapi.hpp
+//
+// Desc: OpenCVR API - OpenCVR API.
+//
+// Copyright (c) 2014-2018 opencvr.com. All rights reserved.
+//------------------------------------------------------------------------------
+#ifndef __VSC_OAPI_H_
+#define __VSC_OAPI_H_
+
+#include "oapiproto.hpp"
+
+
+#endif
