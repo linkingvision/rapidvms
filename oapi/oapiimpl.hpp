3bf01ad90b19d870f68b717b3917a1dea876bd42
xsmart <xsmart@163.com>
2015-04-07 22:57:55 +0800

    add oapi process

---
+++ oapi/oapiimpl.hpp
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// File: oapiimpl.hpp
+//
+// Desc: OpenCVR API - OpenCVR API.
+//
+// Copyright (c) 2014-2018 opencvr.com. All rights reserved.
+//------------------------------------------------------------------------------
+#ifndef __VSC_OAPI_IMPL_H_
+#define __VSC_OAPI_IMPL_H_
+
+
+
+#endif
