3a44a0e19bbeee2a85956444748f6387062ad200
xsmart <xsmart@163.com>
2015-04-20 22:45:59 +0800

    add parse

--- oapi/oapicimpl.hpp
+++ oapi/oapicimpl.hpp
@@ -18,40 +18,77 @@ OAPIClient::~OAPIClient()
 
 }
 
-BOOL OAPIClient::DeviceListRequest(oapi::DeviceList &list)
+
+/* Setup connect with server */
+BOOL OAPIClient::Setup(std::string strUser, std::string strPasswd)
+{
+	//TODO
+	return TRUE;
+}
+
+BOOL OAPIClient::SendDeviceListRequest()
 {
 	OAPIHeader header;
-	header.cmd = htonl(OAPI_CMD_GET_DEVICE_REQ);
+	header.cmd = htonl(OAPI_CMD_DEVICE_LIST_REQ);
 	header.length = 0;
 
 	m_pSocket->Send((void *)&header, sizeof(header));
 
-	s32 nRet = m_pSocket->Recv((void *)&header, sizeof(header));
-	if (nRet == sizeof(header))
-	{
-		header.length = ntohl(header.length);
-		char *pRecv = new char[header.length + 1];
-		s32 nRetBody = m_pSocket->Recv((void *)pRecv, header.length);
-		if (nRetBody == header.length)
-		{
-			std::cerr << pRecv << '\n';
-			autojsoncxx::ParsingResult result;
-			if (!autojsoncxx::from_json_string(pRecv, list, result)) 
-			{
-			    //std::cerr << result << '\n';
-			    //return FALSE;
-			}
-			delete pRecv;
-			
-		}else
-		{
-			delete pRecv;
-			return FALSE;
-		}
-	}else
+	return TRUE;
+
+}
+
+BOOL OAPIClient::StartLiveview(int nId)
+{
+	OAPIHeader header;
+
+	oapi::LiveView liveview;
+	liveview.nId = nId;
+	
+	std::string strJson = autojsoncxx::to_pretty_json_string(liveview);
+	s32 nJsonLen = strJson.length();
+	if (nJsonLen <= 0)
 	{
 		return FALSE;
 	}
+	header.cmd = htonl(OAPI_CMD_START_LIVE);
+	header.length = htonl(nJsonLen + 1);;
+	
+	m_pSocket->Send((void *)&header, sizeof(header));
+	m_pSocket->Send((void *)strJson.c_str(), nJsonLen + 1);
+	return TRUE;
+
 }
 
+BOOL OAPIClient::StopLiveview(int nId)
+{
+	OAPIHeader header;
+
+	oapi::LiveView liveview;
+	liveview.nId = nId;
+	
+	std::string strJson = autojsoncxx::to_pretty_json_string(liveview);
+	s32 nJsonLen = strJson.length();
+	if (nJsonLen <= 0)
+	{
+		return FALSE;
+	}
+	header.cmd = htonl(OAPI_CMD_STOP_LIVE);
+	header.length = htonl(nJsonLen + 1);
+	
+	m_pSocket->Send((void *)&header, sizeof(header));
+	m_pSocket->Send((void *)strJson.c_str(), nJsonLen + 1);
+	return TRUE;
+
+}
+BOOL OAPIClient::ParseDeviceList(char *pRecv, int len, oapi::DeviceList &list)
+{
+	autojsoncxx::ParsingResult result;
+	if (!autojsoncxx::from_json_string(pRecv, list, result)) 
+	{
+	    std::cerr << result << '\n';
+	    return FALSE;
+	}
+	return TRUE;
+}
 #endif
