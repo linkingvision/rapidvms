316b7da82063bd38a4f2b33b1e015b53f8db9b22
xsmart <xsmart@163.com>
2015-05-20 21:34:01 +0800

    change for linux

--- rules.mk
+++ rules.mk
@@ -1,6 +1,10 @@
 export VE_PATH=/home/user/work/opencvr/
-export LD_LIBRARY_PATH=$VE_PATH/linux/lib/:$LD_LIBRARY_PATH
 
+export VE_OS=`lsb_release -si`
+export VE_ARCH=`uname -m | sed 's/x86_//;s/i[3-6]86/32/'`
+export VE_VER=`lsb_release -sr`
+export VE_INSTALL_DIR=$VE_OS-$VE_VER-$VE_ARCH"bit"
+export LD_LIBRARY_PATH=$VE_PATH/output/$VE_INSTALL_DIR/lib/:$LD_LIBRARY_PATH
 
 
 
