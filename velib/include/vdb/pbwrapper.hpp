da6274718426c1db69b75a0fd1ee88bb5ac84444
xsmart <xsmart@163.com>
2015-07-30 00:38:26 +0800

    add pb wrappe

---
+++ velib/include/vdb/pbwrapper.hpp
@@ -0,0 +1,40 @@
+#ifndef __PB_WRAPPER_HPP__
+#define __PB_WRAPPER_HPP__
+#include "utility.hpp"
+#include "mediafile.hpp"
+#include "pbsession.hpp"
+
+#ifdef WIN32
+/* pb callback */
+typedef void (__cdecl * PBCallbackFunctionPtr)(VideoFrame& frame, void * pParam);
+typedef void (__cdecl * PBTimeCallbackFunctionPtr)(u32 currTime, void * pParam);
+#else
+/* pb callback */
+typedef void ( * PBCallbackFunctionPtr)(VideoFrame& frame, void * pParam);
+typedef void ( * PBTimeCallbackFunctionPtr)(u32 currTime, void * pParam);
+#endif
+
+
+class VDB;
+class PlaybackWrapperImpl;
+class VE_LIBRARY_API PlaybackWrapper
+{
+public:
+    PlaybackWrapper(VDB &pVdb, s32 deviceId, u32 nPlaytime, PBCallbackFunctionPtr callback, 
+				void * pParam);
+    ~PlaybackWrapper();
+public:
+	BOOL SeekToTime(u32 seekTime);
+	BOOL StartPlay();
+	BOOL StopPlay();
+	BOOL SetTimeCallback(PBTimeCallbackFunctionPtr callback, void * pParam);
+	
+public:
+	/* main loop for playback */
+	void run();
+	
+private:
+	PlaybackWrapperImpl *m_pImpl;
+};
+
+#endif /* __PB_WRAPPER_HPP__ */
