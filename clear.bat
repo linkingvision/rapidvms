928857e949750618792ec34bef3e3324beda98b6
xsmart <xsmart@veyesys.com>
2015-11-30 20:04:19 +0800

    Signed-off-by: xsmart <xsmart@veyesys.com>

--- clear.bat
+++ clear.bat
@@ -4,6 +4,11 @@ del /s *.~ *.dat *.pdb *.ilk *.pch *.idb *.dep
 del /s *.manifest *.manifest.res *.res *.o
 del /s BuildLog.htm 
 
+rmdir  veuilib\onvifcpplib\win32\Release /s /q
+rmdir  veuilib\onvifcpplib\win32\Debug /s /q
+rmdir  veuilib\onvifcpplib\win32\x64 /s /q
+
+
 FOR /D %%p IN ("xcmnlib\prj_win32\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("veuilib\prj_win32\*.*") DO rmdir "%%p" /s /q
 FOR /D %%p IN ("veuilib\onvifcpplib\prj_win32\*.*") DO rmdir "%%p" /s /q
