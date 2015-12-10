c21ef69eb2f8886dfb3e81287c4d2358440b5c99
xsmart <xsmart@veyesys.com>
2015-12-10 23:02:44 +0800

    Signed-off-by: xsmart <xsmart@veyesys.com>

--- client/src/common/viditem/vscviditemvidstor.cpp
+++ client/src/common/viditem/vscviditemvidstor.cpp
@@ -1,9 +1,9 @@
 #include "common/viditem/vscviditemvidstor.h"
 
-VSCVidItemVidStor::VSCVidItemVidStor(VidStor cStor, QTreeWidgetItem *parent)
-: m_cStor(cStor), VSCVidItemInf(parent)
+VSCVidItemVidStor::VSCVidItemVidStor(VidStor cStor, ClientFactory &pFactory, QTreeWidgetItem *parent)
+: m_cStor(cStor), VSCVidItemInf(pFactory, parent)
 {
-	
+	/* if came here, all the item is ready, just start the cStor client */
 }
 VSCVidItemVidStor::~VSCVidItemVidStor()
 {
