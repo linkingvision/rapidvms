a90c0f0566c479ffb6da95ebf2e7ce0eae3dd835
xsmart <xsmart@veyesys.com>
2016-09-04 12:17:07 +0800

    Signed-off-by: xsmart <xsmart@veyesys.com>

--- rules-osx.mk
+++ rules-osx.mk
@@ -7,5 +7,3 @@ export VE_CROSS_COMPILER=
 export VE_INSTALL_DIR=$VE_OS-$VE_ARCH"bit"
 export LD_LIBRARY_PATH=$VE_PATH/output/$VE_INSTALL_DIR/lib/:$LD_LIBRARY_PATH
 export PATH=$VE_PATH/output/$VE_INSTALL_DIR"/bin:"$PATH 
-
-
