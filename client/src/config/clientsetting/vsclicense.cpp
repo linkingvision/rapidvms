0e86f9a996ed70844e0e677ebee26fad058b51f6
xsmart <xsmart@veyesys.com>
2016-09-16 13:03:27 +0800

    change license from  string to file

--- client/src/config/clientsetting/vsclicense.cpp
+++ client/src/config/clientsetting/vsclicense.cpp
@@ -1,12 +1,16 @@
 #include "debug.hpp"
 #include "config/clientsetting/vsclicense.h"
+#include <fstream>
+#include <iostream>
+#include <QFileDialog>
 
 VSCLicense::VSCLicense(ClientFactory &pFactory, QWidget *parent)
 	: QWidget(parent), m_pFactory(pFactory)
 {
 	ui.setupUi(this);
 	UpdateUI();
-	connect( this->ui.pushButtonApply, SIGNAL( clicked() ), this, SLOT(applyConfig()));
+	connect( this->ui.pushButtonImport, SIGNAL( clicked() ), this, SLOT(SlotImportLic()));
+	connect( this->ui.pushButtonDefault, SIGNAL( clicked() ), this, SLOT(SlotDefaultLic()));
 
 }
 
@@ -31,9 +35,41 @@ void VSCLicense::UpdateUI()
 
 
 
-void VSCLicense::applyConfig()
+void VSCLicense::SlotImportLic()
 {
-	std::string strLicense = ui.lic->toPlainText().toStdString();
+	
+	QFileDialog *fileDialog = new QFileDialog(this); 
+	fileDialog->setWindowTitle(tr("Select License")); 
+	fileDialog->setNameFilter(tr("License(*.lic)")); 
+	QIcon icon;
+	icon.addFile(QStringLiteral(":/logo/resources/vscsmall.png"), QSize(), QIcon::Normal, QIcon::Off);
+	fileDialog->setWindowIcon(icon);
+	if(fileDialog->exec() == QDialog::Accepted) { 
+		QString path = fileDialog->selectedFiles()[0]; 
+		VDC_DEBUG( "%s  QFileDialog %s\n",__FUNCTION__, path.toStdString().c_str());
+		
+		ifstream b_file (path.toStdString().c_str());
+		astring strLic;
+		b_file>> strLic;
+		
+		std::string strLicense = strLic;
+		m_pFactory.SetLicense(strLicense);
+		
+		UpdateUI();	
+		b_file.close();
+	} else 
+	{ 
+	
+	}
+	
+
+	return;
+
+}
+
+void VSCLicense::SlotDefaultLic()
+{
+	std::string strLicense = "qETTpSZ/O4aGoLMNE+oXqXDCtBrIBOHtuvmSnr6clGqcHiMEaI+L1DDOHTR8l03WBE5sYvVenhRDh4WuTMI+ubErKFxJi3l5/pA6+H0FL2nX16issSbA0Pys2zZN4E2R2X9eO+wt2K2g0eomfZXkfwMwuWCFSIGQu84TXCscNS7ID/w2zeIpG8GQ8RY4m2D/QyLKdrRtCfkdYls43pxt9gTpuNgj3e6x3IReEflwgdW5WA9gYMhE6hByqu8iHYPLiL+y7wEn3nPKQmtnHake+x6755+LS4ZszcRjMBRU7Ozxd7rAnImrh6KX408tJc7Gy6C7yPyGxbJyGtOxlC190w==";
 	m_pFactory.SetLicense(strLicense);
 	UpdateUI();
 	return;
